d272fd09f1b6d0954c18f080b2bb11b09a754a9c
GrantMatteo <matteoboy3@gmail.com>
2020-07-10 19:12:14 -0400

    fixed bugs in control script

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controls : MonoBehaviour {
-    public GameObject mech1, mech2, mech2;
-    private GameObject curMech = mech1;
+    public GameObject mech1, mech2, mech3;
+    private GameObject curMech;
     // Start is called before the first frame update
     void Start () {
-
+        curMech = mech1;
     }
 
     // Update is called once per frame
     void Update () {
 
     }
+
     /**
-     * Switch between mechs based on what key was pressed
+     * Switch which mech is being controlled
      */
     void switchContext (int mechNum) {
-        
+        Debug.Assert(mechNum > 0 && mechNum < 4, "Mechnum should be in the range [1,3] but was: " + mechNum);
         switch (mechNum) {
             case 1:
                 curMech = mech1;
