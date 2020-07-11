09126e630307c7952570ac3a1958593460078c62
CDHGit <collinhansen2@gmail.com>
2020-07-10 21:38:34 -0400

    Revert "Merge branch 'working_movement'"
    This reverts commit 8c13100c98f9ac8ffd8be12cfd11c0ab865858f9, reversing
    changes made to 9ac9995f35a4dacecf389270b78ac89f813a74ea.
     Undid Merge of working movement and master because it deleted action context and track scripts

--- Assets/Scripts/Context.cs
+++ Assets/Scripts/Context.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controls : MonoBehaviour {
+public class Context : MonoBehaviour {
     public GameObject mech1, mech2, mech3;
+    public KeyCode mech1Key = KeyCode.A, mech2Key = KeyCode.S, mech3Key = KeyCode.D;
+
     private GameObject curMech;
+    
     // Start is called before the first frame update
     void Start () {
         curMech = mech1;
@@ -12,14 +15,21 @@ public class Controls : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-
+        if (Input.GetKeyDown(mech1Key)){
+            switchContext(1);
+        } else if (Input.GetKeyDown(mech2Key)){
+            switchContext(2);
+        } else if (Input.GetKeyDown(mech3Key)){
+            switchContext(3);
+        }
     }
 
     /**
      * Switch which mech is being controlled
      */
-    void switchContext (int mechNum) {
+    private void switchContext (int mechNum) {
         Debug.Assert(mechNum > 0 && mechNum < 4, "Mechnum should be in the range [1,3] but was: " + mechNum);
+        
         switch (mechNum) {
             case 1:
                 curMech = mech1;
