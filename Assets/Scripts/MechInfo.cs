14afbb2e30420496fe991714d6162a5165efc8af
CDHGit <collinhansen2@gmail.com>
2020-07-10 23:50:17 -0400

    Added Mech Info script to the mechs and basic health stat as well as health bar stuff.

---
+++ Assets/Scripts/MechInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechInfo : MonoBehaviour
+{
+    public int health;
+    // Start is called before the first frame update
+    private Texture2D healthTex;
+    void Start()
+    {
+        healthTex = new Texture2D(1,1);
+        healthTex.SetPixel(0,0,Color.green);
+        healthTex.wrapMode = TextureWrapMode.Repeat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
