2fb3a447668928b71d25ae132e03af32524000ea
CDHGit <collinhansen2@gmail.com>
2020-07-10 20:59:12 -0400

    Working WASD movement for one mech with max speed and no key stop

--- Assets/Scripts/ArrowKeyControls.cs
+++ Assets/Scripts/ArrowKeyControls.cs
@@ -4,32 +4,50 @@ using UnityEngine;
 
 public class ArrowKeyControls : MonoBehaviour
 {
-    public float panSpeed = 20f;
+    public float thrust;
+    public Rigidbody2D rb;
+
+    public float maxSpeed;
     // Start is called before the first frame update
+
+    bool forceApplied = false;
+    float scaleFactor;
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();;
     }
 
     // Update is called once per frame
-    void Update () {
-         
-         Vector3 pos = transform.position;
- 
-         if (Input.GetKey ("w")) {
-             pos.y += panSpeed * Time.deltaTime;
-         }
-         if (Input.GetKey ("s")) {
-             pos.y -= panSpeed * Time.deltaTime;
-         }
-         if (Input.GetKey ("d")) {
-             pos.x += panSpeed * Time.deltaTime;
-         }
-         if (Input.GetKey ("a")) {
-             pos.x -= panSpeed * Time.deltaTime;
-         }
-             
- 
-         transform.position = pos;
+    void FixedUpdate () {
+        forceApplied = false;
+        if (Input.GetKey ("w")) {
+            rb.AddForce(transform.up * thrust);
+            forceApplied = true;
+            // Debug.Log("pressed w");
+        }
+        if (Input.GetKey ("s")) {
+            rb.AddForce(-transform.up * thrust);
+            forceApplied = true;
+            // Debug.Log("pressed s");
+        }
+        if (Input.GetKey ("d")) {
+            rb.AddForce(transform.right * thrust);
+            forceApplied = true;
+            // Debug.Log("pressed d");
+        }
+        if (Input.GetKey ("a")) {
+            rb.AddForce(-transform.right * thrust);
+            forceApplied = true;
+            // Debug.Log("pressed a");
+        }
+        if (!forceApplied) {
+            rb.velocity = Vector3.zero;
+        }
+        Debug.Log(rb.velocity.magnitude);
+        if (rb.velocity.magnitude > maxSpeed) {
+            Debug.Log("maxed speed on mech");
+            scaleFactor = maxSpeed/rb.velocity.magnitude;
+            rb.velocity = rb.velocity * new Vector2(scaleFactor,scaleFactor);
+        }
      }
 }
