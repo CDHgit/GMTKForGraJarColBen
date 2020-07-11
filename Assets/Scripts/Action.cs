73eb47fd65e5b66391d29caf6e64be668cae148a
jibjab910 <jibjab910@gmail.com>
2020-07-11 04:16:08 -0400

    Revert "Merge remote-tracking branch 'origin/master' into pixel_art_forrest"
    This reverts commit 93658019ab336c16c0864be50a4762f2f0584877, reversing
    changes made to 95936377b82947a192b896ceb80517a0c4960361.

--- Assets/Scripts/Action.cs
+++ Assets/Scripts/Action.cs
@@ -2,25 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Action {
-    public abstract void performAction (GameObject o);
-    public static float getAngleToMouse(GameObject o){
-        Vector3 pos = o.GetComponent<Transform> ().position;
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-        Vector2 vec_to_mouse = new Vector2 (mousePos.x - pos.x, mousePos.y - pos.y);
-        float angleBetween = Vector2.SignedAngle (new Vector2 (0, 1), vec_to_mouse);
-        return angleBetween;
-    }
+public interface Action {
+    void performAction ();
 }
 public class TestAction : Action {
-    
-    public override void performAction (GameObject o) {
-        // Debug.Log ("hello"); 
+    public void performAction () {
+        Debug.Log ("hello");
     }
-}
-public class DashAction : Action {
-    public override void performAction (GameObject o) {
-        o.SendMessage("startDash", Action.getAngleToMouse(o));
-    }
-
 }
--- Assets/Scripts/Track.cs
+++ Assets/Scripts/Track.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class Track {
     public int size; // Beats in the queue
-    public Action[] possibleActions = new Action[] { new DashAction(), new TestAction(), new TestAction(), new TestAction(), new TestAction(), new TestAction() }; //potential actions to pull from
+    public Action[] possibleActions = new Action[] { new TestAction () }; //potential actions to pull from
     private Queue actions = new Queue (); //action queue
-    private List<GameObject> mechs = new List<GameObject>();
+
     public Track (int size) {
         this.size = size;
         for (int i = 0; i < size; i++) {
             addAction ();
         }
     }
-    public void addMech(GameObject mech){
-        mechs.Add(mech);
-    }
-    public void removeMech(GameObject mech){
-        mechs.Remove(mech);
-    }
+
     /**
      * Runs the next beat
      */
@@ -26,7 +21,7 @@ public class Track {
         consumeAction ();
         addAction ();
     }
-    
+
     /**
      * Sets the possible actions this Track can pull from
      */
@@ -44,10 +39,7 @@ public class Track {
      * Runs the next action in the queue
      */
     private void consumeAction () {
-        Action a = (Action) actions.Dequeue ();
-        foreach (GameObject m in mechs) { 
-            a.performAction (m);
-        }
+        ((Action) actions.Dequeue ()).performAction ();
     }
 
 }
