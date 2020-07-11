eb393c79992cb373489477e792dd85b653cb4f7d
CDHGit <collinhansen2@gmail.com>
2020-07-11 03:40:13 -0400

    Create bullet stuff done

--- Assets/Scripts/ShootAtActive.cs
+++ Assets/Scripts/ShootAtActive.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class ShootAtActive : MonoBehaviour
 {
+    private Context contextObject;
+    private GameObject curMech;
+
+    private GameObject bullet;
     // Start is called before the first frame update
-    private ArrowKeyControls arrowControlsScript; 
+    public float DegRange;
+    private static Vector3 Vec3Z = new Vector3(0,0,1);
+    public GameObject BulletPrefab;
     void Start()
     {
-        arrowControlsScript = gameObject.GetComponent(ArrowKeyControls);
+        contextObject = GameObject.Find("ContextManager").GetComponent<Context>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Update()
+    {   
+        curMech = contextObject.getCurMech();
+        if(gameObject != curMech){
+            // Get pos of me
+            Vector3 myPos = gameObject.transform.position;
+            // Get vector from this object to the target
+            Vector3 vectorToGameObject = curMech.transform.position - myPos;
+            int DegDither = Random.Range(-(int)DegRange,(int)DegRange);
+            // Dont worry about it unless this breaks
+            // Add rand angle
+            Vector3 ditheredUnitToGameObject = Quaternion.AngleAxis(DegDither, Vec3Z) * vectorToGameObject;
+            // Make unit vector
+            ditheredUnitToGameObject = ditheredUnitToGameObject/ditheredUnitToGameObject.magnitude;
+            // Create bullet ignore collision, add force 2 shoot
+            bullet = Instantiate(BulletPrefab, myPos,  Quaternion.LookRotation(Vector3.forward, vectorToGameObject));
+            Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            bullet.GetComponent<Rigidbody2D>().AddForce(ditheredUnitToGameObject * 1);
+        }
     }
 }
