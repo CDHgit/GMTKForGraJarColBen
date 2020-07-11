a828c3665754231cfeb7df20d772b9437e97b986
GrantMatteo <matteoboy3@gmail.com>
2020-07-10 23:27:52 -0400

    Added track manager, context cooldowns

--- Assets/Scripts/ArrowKeyControls.cs
+++ Assets/Scripts/ArrowKeyControls.cs
@@ -15,7 +15,6 @@ public class ArrowKeyControls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        active = false;
         rb = gameObject.GetComponent<Rigidbody2D>();;
     }
 
